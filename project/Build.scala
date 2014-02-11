ecef7f164f8fe68e40b68b846adeec990632ceec
Dominique Busser <dombusser@gmail.com>
2014-02-11 12:55:29 +0100

    wamplay local

--- project/Build.scala
+++ project/Build.scala
@@ -10,13 +10,14 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
     	javaCore,
       	"com.feth" %% "play-easymail" % "0.5-SNAPSHOT",
-        "ws.wamplay" %% "wamplay" % "0.2.6-SNAPSHOT", // exclude("org.scala-stm", "scala-stm_2.10.0")
+        //"ws.wamplay" %% "wamplay" % "0.2.6-SNAPSHOT", // exclude("org.scala-stm", "scala-stm_2.10.0")
         "com.google.guava" % "guava" % "14.0"
     )
 
     val main = play.Project(name, version, appDependencies).settings(
-      resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
-      resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns),
-      resolvers ++= Seq("Bo's Repo" at "http://blopker.github.com/maven-repo/")
+       resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns)
+      ,resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns)
+      //resolvers ++= Seq("Bo's Repo" at "http://blopker.github.com/maven-repo/")
+      //,resolvers += Resolver.file("Local repo", file(System.getProperty("user.home") + "/.ivy2/local"))(Resolver.ivyStylePatterns)
     )
 }
